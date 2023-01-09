874416c5d22a4d931efe12550d0a2075bfe04e5a
Darya Zhukova <cherepashka2080@gmail.com>
2023-01-09 15:43:14 +0300

    added governance token and time lock contracts

---
+++ contracts/governance/GovernanceToken.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract GovernanceToken is ERC20Votes {
+    uint256 public maxSupply = 1000 * 1000 * 1000000000000000000; // 10^6 eth
+
+    constructor() ERC20("qDAOGovernanceToken", "qGT") ERC20Permit("qDAOGovernanceToken") {
+        _mint(msg.sender, maxSupply);
+    }
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount) internal override(ERC20Votes) {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal override(ERC20Votes) {
+        super._burn(account, amount);
+    }
+}
