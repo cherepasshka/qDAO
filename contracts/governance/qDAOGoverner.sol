135d1ac3e809160dd294e3c2d292c4473ade128b
Darya Zhukova <cherepashka2080@gmail.com>
2023-01-12 15:26:57 +0300

    governor contract: added checks for commission necessity and draft for commission validation

--- contracts/governance/qDAOGoverner.sol
+++ contracts/governance/qDAOGoverner.sol
@@ -10,36 +10,59 @@ import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.so
 
 contract QDAOGovernor is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction, GovernorTimelockControl {
     address[] private commission;
-    
+    mapping(uint256 => bool) private approvedByCommission;
+
     constructor(
         IVotes _token, 
         TimelockController _timelock, 
         uint256 _votingDelay, // delay since proposal is created until voting starts, measured in blocks
         uint256 _votingPeriod, // measured in blocks
         uint256 _proposalThreshold, // minimum number of votes an account must have to create a proposal
-        uint256 _quorumFraction
+        uint256 _quorumFraction,
+        address[] memory _commission
     )
         Governor("qDAO")
         GovernorSettings(_votingDelay, _votingPeriod, _proposalThreshold)
         GovernorVotes(_token)
         GovernorVotesQuorumFraction(_quorumFraction)
         GovernorTimelockControl(_timelock)
-    {}
+    {
+        commission = _commission;
+    }
     
     function execute(
         address[] memory targets,
-        uint256[] memory values,
+        uint256[] memory values, // amount of ethers to pay
         bytes[] memory calldatas,
         bytes32 descriptionHash
     ) public payable override(IGovernor, Governor) returns (uint256) {
         uint256 proposalId = hashProposal(targets, values, calldatas, descriptionHash);
-        if (!super._quorumReached(proposalId) && state(proposalId) == ProposalState.Defeated) { 
-            // too many participants didn't vote, emit commission gathering
+        require(readyToExecute(proposalId), "Proposal is not ready for execution yet, please proceed validation first");
 
-        }
         return super.execute(targets, values, calldatas, descriptionHash);
     }
 
+    function validate(
+        address[] memory targets,
+        uint256[] memory values, // amount of ethers to pay
+        bytes[] memory calldatas,
+        bytes32 descriptionHash
+    ) public view {
+        uint256 proposalId = hashProposal(targets, values, calldatas, descriptionHash);
+        if (isCommissionNeeded(proposalId)) {
+            // emits commission gathering and changes approval state of proposal due to commission decision
+        }
+    }
+
+    function readyToExecute(uint256 proposalId) public view returns(bool) {
+        return (isCommissionNeeded(proposalId) && approvedByCommission[proposalId]) 
+                || !isCommissionNeeded(proposalId);
+    }
+
+    function isCommissionNeeded(uint256 proposalId) public view returns(bool) {
+        return !super._quorumReached(proposalId) && state(proposalId) == ProposalState.Defeated;
+    }
+ 
     // mandatary overrides below
     function votingDelay()
         public
