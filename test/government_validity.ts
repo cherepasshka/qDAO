7faa26f3e009e50e0dd923b0d2a5c91f2fb9912f
Darya Zhukova <cherepashka2080@gmail.com>
2023-01-16 12:56:49 +0300

    minor improvement

--- test/government_validity.ts
+++ test/government_validity.ts
@@ -1,21 +1,22 @@
 import { expect } from "chai";
 // @ts-ignore
 import { ethers, deployments } from "hardhat"
-
-import { CONTRACTS } from "../config/consts"
+import { Contract } from "ethers";
+import {CONTRACTS} from '../config/consts.json'
 
 describe("Government validity", function() {
+    let unit: Contract
+    let governor: Contract
     beforeEach(async function () {
         await deployments.fixture(["all"])
+        unit = await ethers.getContract(CONTRACTS.Unit)
+        governor = await ethers.getContract(CONTRACTS.Governor)
     })
     it("Ownership", async function() {
-        let unit = await ethers.getContract(CONTRACTS.Unit)
         await expect(unit.changeState("new state")).to.be.revertedWith("Ownable: caller is not the owner")
     })
 
     it("Unknown proposal", async function() {
-        let unit = await ethers.getContract(CONTRACTS.Unit)
-        let governor = await ethers.getContract(CONTRACTS.Governor)
         const encodedFunction = unit.interface.encodeFunctionData("changeState", ["new state"])
         const descriptionHash = ethers.utils.id("some description")
         let funcCallPromise = governor.execute([unit.address], [0], [encodedFunction], descriptionHash)
