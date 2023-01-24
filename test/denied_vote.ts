6a1b8506b324d8a9167ca394c8a661043d27a3b5
Darya Zhukova <cherepashka2080@gmail.com>
2023-01-24 04:03:03 +0300

    added test for commission

---
+++ test/denied_vote.ts
@@ -0,0 +1,83 @@
+// scenarios for votes with involving commission
+
+import { assert, expect } from "chai";
+import { mine } from "@nomicfoundation/hardhat-network-helpers";
+import { Contract } from "ethers";
+//@ts-ignore
+import { ethers, deployments, SignerWithAddress } from "hardhat"
+
+import { CONTRACTS, VOTING_DELAY, VOTING_PERIOD, MIN_DELAY } from "../config/consts.json"
+import { CommissionType, getCommission } from "../config/commission";
+import { ProposalState, VoteType } from "../config/enums";
+
+describe("Crisis handling", function() {
+    let unit: Contract
+    let governor: Contract
+    let token: Contract
+    const description = "proposal description"
+    const descriptionHash = ethers.utils.id(description)
+    let addresses: SignerWithAddress[]
+    let commission: CommissionType
+
+    before(async function () {
+        addresses = await ethers.getSigners();
+        commission = await getCommission();
+    })
+
+    beforeEach(async function () {
+        await deployments.fixture(["all"])
+        unit = await ethers.getContract(CONTRACTS.Unit)
+        governor = await ethers.getContract(CONTRACTS.Governor)
+        token = await ethers.getContract(CONTRACTS.GovernanceToken)
+        // distribution tokens among addresses:
+        for (let i = 1; i < addresses.length; ++i) {
+            await token.connect(addresses[i]).delegate(addresses[i].address)
+        }
+        let supply = await token.totalSupply()
+        for (let i = 1; i < addresses.length; ++i) {
+            await token.transfer(addresses[i].address, supply.div(addresses.length * 10))
+        }
+    })
+
+    it("Quorum not reached, fail to execute without commission", async function() {
+        const encodedChangeState = unit.interface.encodeFunctionData("changeState", ["new state"])
+        const proposalTx = await governor.propose([unit.address], [0], [encodedChangeState], description)
+        const proposeReceipt = await proposalTx.wait(1)
+        const proposalId = proposeReceipt.events![0].args!.proposalId
+        assert.equal(await governor.state(proposalId), ProposalState.Pending, "Proposal state expected to be `Pending`")
+        await mine(VOTING_DELAY)
+        for (let i = 1; i < 3; ++i) {
+            await governor.connect(addresses[i]).castVoteWithReason(proposalId, VoteType.Against, "voted for")
+        }
+        for (let i = 3; i < 6; ++i) {
+            await governor.connect(addresses[i]).castVoteWithReason(proposalId, VoteType.Abstain, "voted for")
+        }
+        assert.equal(await governor.state(proposalId), ProposalState.Active, "Proposal state expected to be `Active`")
+        await mine(VOTING_PERIOD)
+        assert.equal(await governor.state(proposalId), ProposalState.Defeated, "Proposal state expected to be `Defeated`")
+        assert.equal(await governor.isCommissionNeeded(proposalId), true, "Commission gathering expected to be neccessary")
+        assert.equal(await governor.noNeedInValidation(proposalId), false, "Proposal is not ready for execution")
+        await expect(governor.execute([unit.address], [0], [encodedChangeState], descriptionHash)).to.be.revertedWith("Proposal need validation")
+
+        // start validating
+        await expect(governor.validate([unit.address], [0], [encodedChangeState], descriptionHash)).not.to.be.reverted
+
+        // sign proposalId by every commission member
+        let signatures: string[] = new Array();
+        let hash =  await governor.decisionHash(proposalId, 1);
+        for (let i = 0; i < commission.members.length; ++i) {
+            let signedDecision = await commission.members[i].signMessage(ethers.utils.arrayify(hash))
+            signatures.push(signedDecision)
+        }
+
+        // submit decision
+        await expect(governor.submit([unit.address], [0], [encodedChangeState], descriptionHash, 1, signatures)).not.to.be.reverted
+
+        assert.equal(await governor.state(proposalId), ProposalState.Succeeded, "Proposal state expected to be `Succeeded`")
+        /* todo:
+            - tests for successfulCommissionGathering
+            - more tests for noNeedInValidation
+            - more tests for submit
+        */
+    })
+})
