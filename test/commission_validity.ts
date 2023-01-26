c855a78d389d44cd68b62981b250d57c0eed9995
Darya Zhukova <cherepashka2080@gmail.com>
2023-01-27 00:35:59 +0300

    added more tests for commission validation

--- test/commission_validity.ts
+++ test/commission_validity.ts
@@ -173,4 +173,105 @@ describe("Invalid commission usecases", function() {
         assert.equal(await governor.noNeedInValidation(proposalId), false, "Validation expected to be necessary")
         assert.equal(await governor.isCommissionNeeded(proposalId), true, "Commission gathering expected to be necessary")
     })
+
+    it("Resubmit solution", async function() {
+        const proposalTx = await governor.propose([unit.address], [0], [encodedChangeState], description)
+        const proposeReceipt = await proposalTx.wait(1)
+        const proposalId = proposeReceipt.events![0].args!.proposalId
+        await mine(VOTING_DELAY)
+        for (let i = 1; i < 3; ++i) {
+            await governor.connect(addresses[i]).castVoteWithReason(proposalId, VoteType.Against, "voted against")
+        }
+        for (let i = 3; i < 6; ++i) {
+            await governor.connect(addresses[i]).castVoteWithReason(proposalId, VoteType.Abstain, "abstain")
+        }
+        await mine(VOTING_PERIOD)
+
+        // start validating
+        await expect(governor.validate([unit.address], [0], [encodedChangeState], descriptionHash)).not.to.be.reverted
+
+        // sign proposalId by every commission member
+        let signatures: string[] = new Array();
+        let hash =  await governor.decisionHash(proposalId, CommissionState.Declined);
+        for (let i = 0; i < commission.members.length; ++i) {
+            let signedDecision = await commission.members[i].signMessage(ethers.utils.arrayify(hash))
+            signatures.push(signedDecision)
+        }
+
+        assert.equal(await governor.successfulCommissionGathering(proposalId), false, "Commission gathering should be in process")
+        // submit decision
+        await expect(governor.submit([unit.address], [0], [encodedChangeState], descriptionHash, CommissionState.Declined, signatures)).not.to.be.reverted
+
+        assert.equal(await governor.state(proposalId), ProposalState.Defeated, "Proposal state expected to be `Defeated`")
+        assert.equal(await governor.successfulCommissionGathering(proposalId), true, "Commission gathering should be successful")
+    
+        await expect(
+            governor.submit([unit.address], [0], [encodedChangeState], descriptionHash, CommissionState.Approved, signatures)
+        ).to.be.revertedWith("Commission has already submited the solution")
+        assert.equal(await governor.state(proposalId), ProposalState.Defeated, "Proposal state expected to be `Defeated`")
+    })
+
+    it("Submit via commission despite commission is unnecessary", async function() {
+        const proposalTx = await governor.propose([unit.address], [0], [encodedChangeState], description)
+        const proposeReceipt = await proposalTx.wait(1)
+        const proposalId = proposeReceipt.events![0].args!.proposalId
+        await mine(VOTING_DELAY)
+        for (let i = 0; i < 3; ++i) {
+            await governor.connect(addresses[i]).castVoteWithReason(proposalId, VoteType.Against, "voted against")
+        }
+        for (let i = 3; i < 6; ++i) {
+            await governor.connect(addresses[i]).castVoteWithReason(proposalId, VoteType.Abstain, "abstain")
+        }
+        await mine(VOTING_PERIOD)
+
+        // start validating
+        assert.equal(await governor.successfulCommissionGathering(proposalId), false, "Commission gathering should be not started")
+        assert.equal(await governor.isCommissionNeeded(proposalId), false, "Commission is unnecessary")
+        
+        // sign proposalId by every commission member
+        let signatures: string[] = new Array();
+        let hash =  await governor.decisionHash(proposalId, CommissionState.Declined);
+        for (let i = 0; i < commission.members.length; ++i) {
+            let signedDecision = await commission.members[i].signMessage(ethers.utils.arrayify(hash))
+            signatures.push(signedDecision)
+        }
+
+        assert.equal(await governor.successfulCommissionGathering(proposalId), false, "Commission gathering should be not started")
+        // submit decision
+        await expect(
+            governor.submit([unit.address], [0], [encodedChangeState], descriptionHash, CommissionState.Declined, signatures)
+        ).to.be.revertedWith("No need for commission solution")
+        assert.equal(await governor.state(proposalId), ProposalState.Defeated, "Proposal state expected to be `Defeated`")
+    })
+
+    it("Submit without initiating commission gathering", async function() {
+        const proposalTx = await governor.propose([unit.address], [0], [encodedChangeState], description)
+        const proposeReceipt = await proposalTx.wait(1)
+        const proposalId = proposeReceipt.events![0].args!.proposalId
+        await mine(VOTING_DELAY)
+        for (let i = 1; i < 3; ++i) {
+            await governor.connect(addresses[i]).castVoteWithReason(proposalId, VoteType.Against, "voted against")
+        }
+        for (let i = 3; i < 6; ++i) {
+            await governor.connect(addresses[i]).castVoteWithReason(proposalId, VoteType.Abstain, "abstain")
+        }
+        await mine(VOTING_PERIOD)
+
+        // sign proposalId by every commission member
+        let signatures: string[] = new Array();
+        let hash =  await governor.decisionHash(proposalId, CommissionState.Declined);
+        for (let i = 0; i < commission.members.length; ++i) {
+            let signedDecision = await commission.members[i].signMessage(ethers.utils.arrayify(hash))
+            signatures.push(signedDecision)
+        }
+
+        assert.equal(await governor.successfulCommissionGathering(proposalId), false, "Commission gathering should be not started")
+        // submit decision
+        await expect(
+            governor.submit([unit.address], [0], [encodedChangeState], descriptionHash, CommissionState.Declined, signatures)
+        ).to.be.revertedWith("No commission gathering was created")
+
+        assert.equal(await governor.state(proposalId), ProposalState.Defeated, "Proposal state expected to be `Defeated`")
+        assert.equal(await governor.successfulCommissionGathering(proposalId), false, "Commission gathering should not be created")
+    })
 })
